bfbc2a1f0eb2371e1ae70828b2613983efac0694
DesktopAleksei <amw694@nyu.edu>
2022-01-28 20:35:11 -0500

    - added functionality for the diorama pieces to move into place

--- Assets/Scripts/Utility/MovePiecesIntoPlace.cs
+++ Assets/Scripts/Utility/MovePiecesIntoPlace.cs
@@ -6,16 +6,43 @@ public class MovePiecesIntoPlace : MonoBehaviour
 {
 
     public List<GameObject> children = new List<GameObject>();
+    public List<Vector3> childrenEndPosition = new List<Vector3>();
+    public List<Vector3> childrenStartPosition = new List<Vector3>();
+
+    public Vector2 randomizeDistance;
+
+    // Movement speed in units per second.
+    public float speed = 100.0F;
+
+    // Time when the movement started.
+    private float startTime;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
         getAllRelevantChildren();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - startTime) * speed;
+
+        for(var i = 0; i < children.Count; i ++)
+        {
+            if(!children[i].activeSelf)
+            {
+                children[i].SetActive(true);
+            }
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fractionOfJourney = distCovered / Vector3.Distance(childrenStartPosition[i], childrenEndPosition[i]); ;
+
+            // Set our position as a fraction of the distance between the markers.
+            children[i].transform.position = Vector3.Lerp(childrenStartPosition[i], childrenEndPosition[i], fractionOfJourney);
+        }
     }
 
     void getAllRelevantChildren()
@@ -26,7 +53,13 @@ public class MovePiecesIntoPlace : MonoBehaviour
             Transform currentChild = currentTransform.GetChild(i);
             for (var j = 0; j < currentChild.childCount; j++)
             {
-
+                Transform grandChild = currentChild.GetChild(j);
+                children.Add(grandChild.gameObject);
+                childrenEndPosition.Add(grandChild.position);
+                childrenStartPosition.Add(new Vector3(grandChild.position.x + Random.Range(randomizeDistance.x, randomizeDistance.y),    //x
+                    grandChild.position.y + Random.Range(randomizeDistance.x, randomizeDistance.y),                                      //y
+                    grandChild.position.z + Random.Range(randomizeDistance.x, randomizeDistance.y)));                                    //z
+                grandChild.gameObject.SetActive(false);
             }
         }
     }
