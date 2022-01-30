6dd7e0fcec1a614a8ec02eb8bacc65e74a56882c
Calvin So <ccs403@nyu.edu>
2022-01-30 13:18:14 -0500

    Added a volume to remove chromatic aberration. Used in final scene for text legibility Added billboards at the end that show you the information you've read up to this point.

---
+++ Assets/Scripts/Controllers/BillboardController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BillboardController : MonoBehaviour
+{
+    private enum BillboardSide
+    {
+        None = 0,
+        Left = 1,
+        Right = 2
+    }
+
+    [SerializeField]
+    private BillboardSide side = BillboardSide.None;
+
+    [SerializeField]
+    private TextMeshProUGUI textBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Init list
+        List<string> lines;
+
+        //Grabs lines based on side
+        switch (side)
+        {
+            case BillboardSide.Left:
+                lines = Singleton.Instance.GetComponent<AllPairedTextHolder>().getFirstCharaLines();
+                break;
+            case BillboardSide.Right:
+                lines = Singleton.Instance.GetComponent<AllPairedTextHolder>().getSecondCharaLines();
+                break;
+            case BillboardSide.None:
+            default:
+                Debug.LogError("BillboardController Error: Billboard side not set.");
+                return;
+        }
+
+        string outputString = "";
+
+        foreach (string sentence in lines)
+        {
+            outputString += "- " + sentence + "\n";
+        }
+
+        textBox.text = outputString;
+    }
+}
